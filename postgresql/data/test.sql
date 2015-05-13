d5b7bf2fbca1b8d395da63523c5ee3f79c2d05a2
Tommy Wang <tommy@gaixie.org>
2015-05-13 17:49:19 +0800

    修改测试数据的邮箱

--- postgresql/data/test.sql
+++ postgresql/data/test.sql
@@ -1,12 +1,14 @@
 -- -----------------------------------------------------------------------------
 -- MD5 ("111111") = 96e79218965eb72c92a549dd5a330112
 -- 注册邀请顺序：Manager --> Sponsor --> Register
-insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('test1','96e79218965eb72c92a549dd5a330112','Manager', 4,'test1@gaixie.org',currval('userbase_id_seq'));
-insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('test2','96e79218965eb72c92a549dd5a330112','Sponsor', 2,'test2@gaixie.org',(select id from userbase where username = 'test1'));
-insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('test3','96e79218965eb72c92a549dd5a330112','Register',1,'test3@gaixie.org',(select id from userbase where username = 'test2'));
+insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('qq','96e79218965eb72c92a549dd5a330112','Manager', 4,'nodto@qq.com',currval('userbase_id_seq'));
+insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('sina','96e79218965eb72c92a549dd5a330112','Sponsor', 2,'nodto@sina.com',(select id from userbase where username = 'qq'));
+insert into userbase(username,password,fullname,type,emailaddress,invited_by) values ('outlook','96e79218965eb72c92a549dd5a330112','Register',1,'nodto@outlook.com',(select id from userbase where username = 'sina'));
 
 -- -----------------------------------------------------------------------------
 -- MD5 ("password") = 5f4dcc3b5aa765d61d8327deb882cf99
 -- MD5 ("register") = 9de4a97425678c5b1288aa70c1669a64
-insert into tokens(value,type,expiration_ts,send_to,created_by) values ('5f4dcc3b5aa765d61d8327deb882cf99','password',now() + interval '1 day','test3@gaixie.org',(select id from userbase where username = 'test3'));
-insert into tokens(value,type,expiration_ts,send_to,created_by) values ('9de4a97425678c5b1288aa70c1669a64','register',now() + interval '7 day','test5@gaixie.org',(select id from userbase where username = 'test2'));
+-- MD5 ("signin")   = cc0256df40cbc924af2b31aeccb869b0
+insert into tokens(value,type,expiration_ts,send_to,created_by) values ('5f4dcc3b5aa765d61d8327deb882cf99','password',now() + interval '1 day' ,'nodto@outlook.com',(select id from userbase where username = 'outlook'));
+insert into tokens(value,type,expiration_ts,send_to,created_by) values ('9de4a97425678c5b1288aa70c1669a64','register',now() + interval '7 day' ,'nodto@test',(select id from userbase where username = 'sina'));
+insert into tokens(value,type,expiration_ts,send_to,created_by) values ('cc0256df40cbc924af2b31aeccb869b0','signin'  ,now() + interval '14 day','nodto@qq.com',(select id from userbase where username = 'qq'));
